4a9b606f160c8fa50054762655d1ddc0bb0902f8
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2020-07-01 10:26:14 +0600

    last call of java srcipt

--- lab6/Javascript/htmlcssjs.js
+++ lab6/Javascript/htmlcssjs.js
@@ -4,4 +4,11 @@ imgElement.onclick = function(){
     var mainImage = document.getElementById('mainImage');
     var imgaesrcAttribute = imgElement.getAttribute('src')
     mainImage.setAttribute('src',imgaesrcAttribute)
-}
+};
+
+var about = document.getElementById('about');
+about.onclick = function (){
+    document.getElementById('h1').innerHTML = ' this is aobut'
+    document.getElementById('p').innerHTML = ' this is aobut'
+    document.getElementById('img').setAttribute('src','img/image2.jpg' )
+};
